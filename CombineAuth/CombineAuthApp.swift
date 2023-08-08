e650e2d70217fc5ae20f76c43c2aa0d9d4924194
Alexey Kolch <kolychenkov94@mail.ru>
2023-08-08 14:37:23 +0300

    Initial Commit

---
+++ CombineAuth/CombineAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAuthApp.swift
+//  CombineAuth
+//
+//  Created by Алексей Колыченков on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
